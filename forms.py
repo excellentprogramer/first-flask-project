eeadce5903747e4948125cd9fe644b19ff0d778f
fahro <fahrudin@occrp.org>
2017-10-30 17:20:08 +0100

    signup form

---
+++ forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import Form
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class SignupForm(Form):
+    first_name = StringField("First name", validators=[DataRequired()])
+    last_name = StringField("Last name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Sign up")
--- routes.py
+++ routes.py
@@ -1,15 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from models import db
+from forms import SignupForm
+
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
+db.init_app(app)
+
+app.secret_key = 'development-key'
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/about")
 def about_page():
     return render_template("about.html")
 
 
-if __name__ =="__main__":
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if request.method == 'POST':
+        if form.validate() is False:
+            return render_template('signup.html', form=form)
+        return 'Success'
+    elif request.method == "GET":
+        return render_template('signup.html', form=form)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
